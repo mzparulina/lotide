6bc5350a92dee0c70b4542b3da93686fffb035e8
mzparulina <pauline.revilla@gmail.com>
2022-11-21 12:51:40 -0500

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  else {
+    // Comparing each element of your array
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let equals = eqArrays(arr1, arr2);
+  let log = '';
+  if (equals)
+    log += `✅  Assertion passed: ${arr1} === ${arr2}`;
+  else
+    log += `🛑  Assertion failed: ${arr1} !== ${arr2}`;
+  return console.log(log);
+};
+
+// TEST CODE
+assertArraysEqual(["1", "2", "3"], ["1", "2", '3']);
--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let log = '';
-  if (actual === expected && actual && expected)
-    log += `✅  Assertion passed: ${actual} === ${expected}`;
-  else
-    log += `🛑  Assertion failed: ${actual} !== ${expected}`;
-  return console.log(log);
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   else {
@@ -20,11 +11,10 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should PASS

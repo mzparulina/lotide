4d92bd3388518e315c2d7cd47440edf063050d92
mzparulina <pauline.revilla@gmail.com>
2022-11-21 23:04:46 -0500

    Applied eslint fix

--- eqArrays.js
+++ eqArrays.js
@@ -14,7 +14,7 @@ const eqArrays = function(arr1, arr2) {
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // false
 
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should PASS
--- middle.js
+++ middle.js
@@ -36,16 +36,16 @@ const middle = function(arr) {
 
 // TEST CODE
 // ...
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-console.log('--------')
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]),[2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4])
+middle([1]); // => []
+middle([1, 2]); // => []
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+console.log('--------');
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]),[2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);

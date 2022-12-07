0f32688b74ce1d5d4f5f64e0b8f4bc10375deafc
mzparulina <pauline.revilla@gmail.com>
2022-12-07 09:07:11 -0500

    Added flattenTest

--- flatten.js
+++ flatten.js
@@ -1,5 +1,15 @@
-const flatten = function(arr) {
-  return [].concat(...arr);
+const flatten = function(array) {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (const element of array[i]) {
+        flattenedArray.push(element);
+      }
+    } else {
+      flattenedArray.push(array[i]);
+    }
+  }
+  return flattenedArray;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+module.exports = flatten;
